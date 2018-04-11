9124524dbfd0b764d05c8ff19f663cbfcc1634ff
Pep Salcedo <salcedo.nerv@gmail.com>
2018-04-10 22:33:52 -0500

    fisrt commit

---
+++ Practices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practices
+//
+//  Created by Jose Martin Salcedo Lazaro on 08/04/18.
+//  Copyright © 2018 Jose Martin Salcedo Lazaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
